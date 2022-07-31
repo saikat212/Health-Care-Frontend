9c1f1a69cdb8dd1e2724ee6dd26982eac180c16a
Saikat <saikat212saikat@gmail.com>
2022-07-31 12:50:22 +0600

    data load code enable in spacilities.tsx file

--- src/specialities.tsx
+++ src/specialities.tsx
@@ -8,62 +8,62 @@ import {
     CardMedia,
     Grid,
     Typography,
-} from "@mui/material";
-
-import React, {useEffect} from "react";
-import {Link, useNavigate} from "react-router-dom";
-import {API} from "./API Handler/api";
-import {Speciality} from "./Classes/patient-class";
-import Header from "./components/header";
-import SpecialityCard from "./components/speciality-card";
-
-/* function LoopLoop(){
-    let jsx = "";
-    let i;
-    for(i=0; i<100; i++){
-        jsx += 
-    }
-
-
-} */
-export function SpecialitiesUI() {
-    const [state, setState] = React.useState<Speciality[]>();
-
+  } from "@mui/material";
+  
+  import { useEffect } from "react";
+  import { Link, useNavigate } from "react-router-dom";
+  import { API } from "./API Handler/api";
+  import { Speciality } from "./Classes/patient-class";
+  import Header from "./components/header";
+  import SpecialityCard from "./components/speciality-card";
+  import * as React from 'react'
+  /* function LoopLoop(){
+      let jsx = "";
+      let i;
+      for(i=0; i<100; i++){
+          jsx += 
+      }
+  
+  
+  } */
+  export function SpecialitiesUI() {
+    const [state,setState] = React.useState<Speciality[]>();
+    
     useEffect(() => {   //Data will be loaded first time only
-        API.speciality.getSpecialities().then(response => {
-            setState(response.data);
-        })
+      API.speciality.getSpecialities().then(response=>{
+        setState(response.data);
+     })
     }, [])
-
-
+    
+   
     return (
-        <Grid container spacing={2} sx={{backgroundColor: "gray"}}>
-            <Grid item>
-                <Header/>
-            </Grid>
-            <Grid item>
-                <Grid container spacing={2}>
-                    {/* {state?.map((speciality, idx) => (
-                        <Grid key={idx} item xs={12} sm={6} md={4} lg={3} xl={2}>
-
-                            <SpecialityCard
-
-                                title={speciality.name}
-                                description={speciality.description}
-
-                                image={require("./images/heaa.jpg")}
-                            />
-
-                        </Grid>
-                    ))} */}
-                </Grid>
-            </Grid>
-
-            {/* {<ButtonGroup variant="contained" aria-label="outlined primary button group">
-      <Button size="large">Medicine/General Physician</Button>
-      <Button >Two</Button>
-      <Button>Three</Button>
-    </ButtonGroup>} */}
+      <Grid container spacing={2} sx={{ backgroundColor: "gray" }}>
+        <Grid item>
+          <Header />
         </Grid>
+        <Grid item>
+          <Grid container spacing={2}>
+            {state?.map((speciality,idx) => (
+              <Grid key={idx} item xs={12} sm={6} md={4} lg={3} xl={2}>
+                 
+                <SpecialityCard 
+                 
+                  title={speciality.name}
+                  description={speciality.description}
+                  
+                  image={require("./images/heaa.jpg")}
+                />
+            
+              </Grid>
+            ))}
+          </Grid>
+        </Grid>
+  
+        {/* {<ButtonGroup variant="contained" aria-label="outlined primary button group">
+        <Button size="large">Medicine/General Physician</Button>
+        <Button >Two</Button>
+        <Button>Three</Button>
+      </ButtonGroup>} */}
+      </Grid>
     );
-}
+  }
